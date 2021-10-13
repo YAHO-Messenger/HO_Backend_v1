687e3c8867b6aa83c3285f3f485f636896e6f29e
heesung33 <chaeheesung33@gmail.com>
2021-10-13 22:43:15 +0900

    🌌 :: 프로젝트 생성

---
+++ src/main/java/com/yaho/ho_backend_v1/HoBackendV1Application.java
@@ -0,0 +1,13 @@
+package com.yaho.ho_backend_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoBackendV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoBackendV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/yaho/ho_backend_v1/HoBackendV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaho.ho_backend_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoBackendV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
