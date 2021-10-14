890a848db75a510ac1cdc248544e862a415762e8
heesung33 <chaeheesung33@gmail.com>
2021-10-14 16:11:40 +0900

    ♻️ :: 오타 수정

--- src/main/java/com/yaho/ho_backend_v1/entity/User/UserRepository.java
+++ src/main/java/com/yaho/ho_backend_v1/entity/User/UserRepository.java
@@ -1,10 +1,7 @@
 package com.yaho.ho_backend_v1.entity.User;
 
-import com.yaho.ho_backend_v1.entity.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 
 @Repository
