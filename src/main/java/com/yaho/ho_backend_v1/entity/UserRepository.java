dcf854a5f552749c854727d5b395a6c3d278272c
heesung33 <chaeheesung33@gmail.com>
2021-10-14 09:34:24 +0900

    🔧 :: User

---
+++ src/main/java/com/yaho/ho_backend_v1/entity/UserRepository.java
@@ -0,0 +1,10 @@
+package com.yaho.ho_backend_v1.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
