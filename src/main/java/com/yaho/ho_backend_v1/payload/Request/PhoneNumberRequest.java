0e970ef990aeac60087cf7cbcf4322d29ded748d
heesung33 <chaeheesung33@gmail.com>
2021-10-14 22:35:34 +0900

    🔧 :: sms 인증 요청

---
+++ src/main/java/com/yaho/ho_backend_v1/payload/Request/PhoneNumberRequest.java
@@ -0,0 +1,10 @@
+package com.yaho.ho_backend_v1.payload.Request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PhoneNumberRequest {
+    private String phoneNumber;
+}
