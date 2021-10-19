0be75524862ae081b5f86dbda9b5764299f8f99c
heesung33 <chaeheesung33@gmail.com>
2021-10-19 20:05:54 +0900

    🔧 :: login 기능 추가

--- src/main/java/com/yaho/ho_backend_v1/payload/Request/LoginRequest.java
+++ src/main/java/com/yaho/ho_backend_v1/payload/Request/LoginRequest.java
@@ -1,2 +1,13 @@
-package com.yaho.ho_backend_v1.payload.Request;public class LoginRequest {
+package com.yaho.ho_backend_v1.payload.Request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class LoginRequest {
+    private String email;
+    private String pw;
 }
