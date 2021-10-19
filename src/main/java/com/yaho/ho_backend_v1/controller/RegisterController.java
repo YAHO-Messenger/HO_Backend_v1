4ba4c50d756731c2154ee99374edb66e19fc96d7
heesung33 <chaeheesung33@gmail.com>
2021-10-19 20:02:06 +0900

    🔥 :: 기능 삭제 및 수정

--- src/main/java/com/yaho/ho_backend_v1/controller/RegisterController.java
+++ src/main/java/com/yaho/ho_backend_v1/controller/RegisterController.java
@@ -3,20 +3,20 @@ package com.yaho.ho_backend_v1.controller;
 import com.yaho.ho_backend_v1.payload.Request.RegisterRequest;
 import com.yaho.ho_backend_v1.service.Register.RegisterService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.Random;
 
 @RestController
 @RequiredArgsConstructor
-public class RegisterController {
+public class AuthController {
     private final RegisterService registerService;
 
     @PostMapping("/Register")
     public String register(@RequestBody @Valid RegisterRequest request) {
         return registerService.register(request);
     }
-
 
 }
