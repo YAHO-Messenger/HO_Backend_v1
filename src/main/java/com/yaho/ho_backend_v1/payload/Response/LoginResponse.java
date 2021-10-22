389e467ed0c9f7b8655c86cb154a2eed88580b42
heesung33 <chaeheesung33@gmail.com>
2021-10-22 18:20:26 +0900

    🔧 :: 응답 기능 추가

--- src/main/java/com/yaho/ho_backend_v1/payload/Response/LoginResponse.java
+++ src/main/java/com/yaho/ho_backend_v1/payload/Response/LoginResponse.java
@@ -1,2 +1,14 @@
-package com.yaho.ho_backend_v1.payload.Response;public class LoginResponse {
+package com.yaho.ho_backend_v1.payload.Response;
+
+import com.yaho.ho_backend_v1.entity.User.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class LoginResponse {
+    private boolean isSuccess;
+    private User user;
 }
--- src/main/java/com/yaho/ho_backend_v1/payload/Response/TokenResponse.java
+++ src/main/java/com/yaho/ho_backend_v1/payload/Response/TokenResponse.java
@@ -1,2 +1,14 @@
-package com.yaho.ho_backend_v1.payload.Response;public class TokenResponse {
+package com.yaho.ho_backend_v1.payload.Response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenResponse {
+    private String acessToken;
+    private String refreshToken;
+    private String tokenexpired;
 }
