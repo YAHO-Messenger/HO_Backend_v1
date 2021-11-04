9dedc550957bbe99a2078f9b2dc4f15d915f575f
heesung33 <chaeheesung33@gmail.com>
2021-11-04 16:40:12 +0900

    🔧 :: 채팅 handler 구현

---
+++ src/main/java/com/yaho/ho_backend_v1/handler/WebSocketHandler.java
@@ -0,0 +1,29 @@
+package com.yaho.ho_backend_v1.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yaho.ho_backend_v1.dto.ChatMessage;
+import com.yaho.ho_backend_v1.dto.ChatRoom;
+import com.yaho.ho_backend_v1.service.websocket.ChatService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+    private final ObjectMapper objectMapper;
+    private final ChatService chatService;
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload();
+        log.info("payload {}", payload);
+        ChatMessage chatMessage = objectMapper.readValue(payload, ChatMessage.class);
+        ChatRoom room = chatService.findRoomById(chatMessage.getRoomId());
+        room.handleActions(session, chatMessage, chatService);
+    }
+}
