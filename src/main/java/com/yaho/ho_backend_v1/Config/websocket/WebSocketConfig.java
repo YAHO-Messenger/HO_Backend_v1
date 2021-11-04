2bba5afd4e578e1f545f98719910fb8c424951c8
heesung33 <chaeheesung33@gmail.com>
2021-11-04 16:41:00 +0900

    🔧 :: WebSocket 구현

---
+++ src/main/java/com/yaho/ho_backend_v1/Config/websocket/WebSocketConfig.java
@@ -0,0 +1,20 @@
+package com.yaho.ho_backend_v1.Config.websocket;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    private final WebSocketHandler webSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler, "/ws/chat").setAllowedOrigins("*");
+    }
+}
