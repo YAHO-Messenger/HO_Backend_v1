89766eb894223720be9e31c2ee3b3ff63212a3e3
heesung33 <chaeheesung33@gmail.com>
2021-11-04 16:39:06 +0900

    🔧 :: chat room 생성 구현

---
+++ src/main/java/com/yaho/ho_backend_v1/controller/ChatController.java
@@ -0,0 +1,26 @@
+package com.yaho.ho_backend_v1.controller;
+
+import com.yaho.ho_backend_v1.dto.ChatRoom;
+import com.yaho.ho_backend_v1.service.websocket.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/chat")
+public class ChatController {
+
+    private final ChatService chatService;
+
+    @PostMapping
+    public ChatRoom createRoom(@RequestParam String name) {
+        return chatService.createRoom(name);
+    }
+
+    @GetMapping
+    public List<ChatRoom> findAllRoom() {
+        return chatService.findAllRoom();
+    }
+}
