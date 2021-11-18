3af1ac07dc5d6f89793f83744e63b67a83fa967f
heesung33 <chaeheesung33@gmail.com>
2021-11-18 13:46:17 +0900

    🔧 :: cors 추가

---
+++ src/main/java/com/yaho/ho_backend_v1/config/web/WebConfig.java
@@ -0,0 +1,16 @@
+package com.yaho.ho_backend_v1.config.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*");
+    }
+
+}
