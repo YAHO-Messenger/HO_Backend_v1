70151620da221482219cbe0ac599db63a6fb1589
heesung33 <chaeheesung33@gmail.com>
2021-11-22 18:29:08 +0900

    [DELETE] login, signup 전체 삭제

--- src/main/java/com/yaho/ho_backend_v1/service/register/RegisterService.java
+++
@@ -1,7 +0,0 @@
-package com.yaho.ho_backend_v1.service.register;
-
-import com.yaho.ho_backend_v1.payload.RegisterRequest;
-
-public interface RegisterService {
-    String register(RegisterRequest request);
-}
