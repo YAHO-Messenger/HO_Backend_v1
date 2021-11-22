378fcf8a41ba38236d10f4cd71436b1e7c070cad
heesung33 <chaeheesung33@gmail.com>
2021-11-22 21:20:42 +0900

    [UPDATE] SignupRequest

--- src/main/java/com/yaho/ho_backend_v1/payload/request/SignupRequest.java
+++ src/main/java/com/yaho/ho_backend_v1/payload/request/SignupRequest.java
@@ -14,7 +14,6 @@ public class SignupRequest {
     @Email
     private String email;
 
-    private Set<String> role;
 
     @NotBlank
     @Size(min = 6, max = 40)
@@ -44,11 +43,5 @@ public class SignupRequest {
         this.password = password;
     }
 
-    public Set<String> getRole() {
-        return this.role;
-    }
 
-    public void setRole(Set<String> role) {
-        this.role = role;
-    }
 }
