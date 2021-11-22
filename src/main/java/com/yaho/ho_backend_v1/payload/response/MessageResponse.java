9473ae691c182e0c1f0ef816e17762d672980b05
heesung33 <chaeheesung33@gmail.com>
2021-11-22 18:31:32 +0900

    [CREATE] Response 설정

---
+++ src/main/java/com/yaho/ho_backend_v1/payload/response/MessageResponse.java
@@ -0,0 +1,17 @@
+package com.yaho.ho_backend_v1.payload.response;
+
+public class MessageResponse {
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
