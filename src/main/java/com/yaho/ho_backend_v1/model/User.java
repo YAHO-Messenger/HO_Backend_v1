aa5b4e453cfeca5a4f524bc3c82ff33c4d98ca83
heesung33 <chaeheesung33@gmail.com>
2021-12-20 20:16:44 +0900

    [UPDATE] User

--- src/main/java/com/yaho/ho_backend_v1/model/User.java
+++ src/main/java/com/yaho/ho_backend_v1/model/User.java
@@ -5,7 +5,6 @@ package com.yaho.ho_backend_v1.model;
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
