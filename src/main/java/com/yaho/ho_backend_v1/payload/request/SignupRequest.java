1c9c784da28ff86fb8beb342a8f53c4fa15f5a2e
heesung33 <chaeheesung33@gmail.com>
2021-12-20 20:16:38 +0900

    [UPDATE] SignUpRequest

--- src/main/java/com/yaho/ho_backend_v1/payload/request/SignupRequest.java
+++ src/main/java/com/yaho/ho_backend_v1/payload/request/SignupRequest.java
@@ -6,9 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-import java.util.Set;
 
 
 @Getter
