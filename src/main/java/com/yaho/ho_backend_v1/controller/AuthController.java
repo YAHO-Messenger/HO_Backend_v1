286a8218ff30c3835483e2e138b37e541455cfa4
heesung33 <chaeheesung33@gmail.com>
2022-03-10 12:21:09 +0900

    test

--- src/main/java/com/yaho/ho_backend_v1/controller/AuthController.java
+++ src/main/java/com/yaho/ho_backend_v1/controller/AuthController.java
@@ -73,7 +73,7 @@ public class AuthController {
                 userDetails.getUsername(), userDetails.getEmail(), roles));
     }
 
-    @PostMapping("/register")
+    @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
         if (userRepository.existsByUsername(signUpRequest.getUsername())) {
             return ResponseEntity.badRequest().body(new MessageResponse("Error: Username is already taken!"));
